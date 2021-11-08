4f17bc9af7fa5ab0b8d14dfd9c697d386e05de84
Marcin Wyszynski <marcin.pixie@gmail.com>
2021-11-08 11:00:48 +0100

    Version number

--- pet/main.tf
+++ pet/main.tf
@@ -1,9 +1,9 @@
-variable "version" {
+variable "version_number" {
   default = 1
 }
 
 resource "random_pet" "this" {
   keepers = {
-    version = var.version
+    version = var.version_number
   }
 }
--- uuid/main.tf
+++ uuid/main.tf
@@ -1,9 +1,9 @@
-variable "version" {
+variable "version_number" {
   default = 1
 }
 
 resource "random_uuid" "this" {
   keepers = {
-    version = var.version
+    version = var.version_number
   }
 }
